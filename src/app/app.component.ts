c75a59d659d44e3f9fbeee292fffbec5cf3d338c
Aitor Baragaño <aitorbf@gmail.com>
2018-02-16 15:08:57 +0100

    Initialize articles to an empty list and sort articles by votes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,7 @@ export class AppComponent {
   articles: Article[];
 
   constructor() {
-    this.articles = [
-        new Article('Angular 2', 'http://angular.io', 3),
-        new Article('Fullstack', 'http://fullstack.io', 2),
-        new Article('Angular Homepage', 'http://angular.io', 1)
-    ];
+    this.articles = [];
   }
 
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
@@ -23,6 +19,10 @@ export class AppComponent {
     title.value = '';
     link.value = '';
     return false;
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
   }
 
 }
